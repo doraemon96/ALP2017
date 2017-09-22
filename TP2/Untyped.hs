e6103dffe286919fc559813f6db4eeb7c2313dda
Nicolas S <soncininicolas@gmail.com>
2017-09-22 16:35:03 -0300

    AAAAAAAAAAA

--- TP2/Untyped.hs
+++ TP2/Untyped.hs
@@ -30,7 +30,7 @@ shift :: Term -> Int -> Int -> Term
 shift (Bound n) c d = if n < c
                       then (Bound n)
                       else (Bound (n+d))
---shift (Free x) c d  = (Free x) PREGUNTAR
+shift (Free x) c d  = Free x
 shift (p :@: q) c d = (shift p c d) :@: (shift q c d)
 shift (Lam p) c d   = Lam (shift p (c+1) d)
   
@@ -39,10 +39,18 @@ subst :: Term -> Term -> Int -> Term
 subst (Bound n) r i = if n == i
                       then r
                       else (Bound n)
---subst (Free x) r i  = Free x PREGUNTAR
+subst (Free x) r i  = Free x
 subst (p :@: q) r i = (subst p r i) :@: (subst q r i)
 subst (Lam p) r i   = Lam (subst p (shift r 0 1) (i+1))
 
+
 eval :: NameEnv Term -> Term -> Term
-eval = undefined
-    
+eval env (Free x)           = case lookup x env of
+                              Just y  -> y 
+                              Nothing -> (Free x)
+eval env (Bound x)         = Bound x
+eval env ((Free x) :@: q)  = eval env ((eval env (Free x)) :@: q)
+eval env ((Bound n) :@: q) = (Bound n) :@: (eval env q)
+eval env ((x :@: y) :@: q) = (x :@: y) :@: (eval env q)
+eval env (Lam p)           = Lam (eval env p)
+eval env ((Lam p) :@: q)   = eval env (shift (subst p (shift q 0 1) 0) 0 (-1))
